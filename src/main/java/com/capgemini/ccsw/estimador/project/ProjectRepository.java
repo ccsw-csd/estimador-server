5e4337024915a852839e25421e296b6ac241db0d
Armen <100298950+armirzoya@users.noreply.github.com>
2022-02-24 12:55:11 +0100

    Query generation improvement

--- src/main/java/com/capgemini/ccsw/estimador/project/ProjectRepository.java
+++ src/main/java/com/capgemini/ccsw/estimador/project/ProjectRepository.java
@@ -2,6 +2,7 @@ package com.capgemini.ccsw.estimador.project;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import com.capgemini.ccsw.estimador.project.model.ProjectEntity;
@@ -15,5 +16,6 @@ public interface ProjectRepository extends CrudRepository<ProjectEntity, Long>{
 	 * Metodo para recuperar un listado de {@link com.capgemini.ccsw.estimador.project.model.ProjectEntity}
 	 * @return
 	 */
+	@EntityGraph(attributePaths = {"customer","createdBy"})
 	List<ProjectEntity> findAll();
 }
