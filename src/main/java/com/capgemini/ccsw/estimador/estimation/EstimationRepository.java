188e1a052df767829db42fad5bf8c48e2dd0bdfa
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2022-02-25 08:43:13 +0100

    Añadido entityGraph para acelerar la consulta

--- src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationRepository.java
+++ src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationRepository.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,17 +14,17 @@ import com.capgemini.ccsw.estimador.estimation.model.EstimationEntity;
 /**
  * @author iciudade
  */
-public interface EstimationRepository extends CrudRepository<EstimationEntity, Long>{
+public interface EstimationRepository extends CrudRepository<EstimationEntity, Long> {
 
-	/**
-	 * Metodo para recuperar un listado paginado de {@link com.capgemini.ccsw.estimador.estimation.model.EstimationEntity}
-	 * @param pageable
-	 * @return
-	 */
-	@Query("select e FROM EstimationEntity e "
-			+ "WHERE (:customer is null or e.project.customer.id = :customer) "
-			+ "and (:project is null or e.project.name like '%'||:project||'%') "
-			+ "and (:startDate is null or :endDate is null or e.created BETWEEN :startDate and :endDate)")
-	Page<EstimationEntity> find(@Param("customer") Long customer, @Param("project") String project, 
-								@Param("startDate") Date startDate, @Param("endDate") Date endDate, Pageable pageable);
+   /**
+    * Metodo para recuperar un listado paginado de {@link com.capgemini.ccsw.estimador.estimation.model.EstimationEntity}
+    * @param pageable
+    * @return
+    */
+   @Query("select e FROM EstimationEntity e " + "WHERE (:customer is null or e.project.customer.id = :customer) "
+         + "and (:project is null or e.project.name like '%'||:project||'%') "
+         + "and (:startDate is null or :endDate is null or e.created BETWEEN :startDate and :endDate)")
+   @EntityGraph(attributePaths = { "project", "project.customer", "createdBy", "createdBy.role" })
+   Page<EstimationEntity> find(@Param("customer") Long customer, @Param("project") String project, @Param("startDate") Date startDate,
+         @Param("endDate") Date endDate, Pageable pageable);
 }
