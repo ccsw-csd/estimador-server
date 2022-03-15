1b64d93f76ec8960216c7cc61a99f9164fd0d388
CORP\asolerpa <andrea.soler-paz-external@capgemini.com>
2022-03-15 11:58:11 +0100

    Creación método que devuelve una estimación por su id

--- src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationController.java
+++ src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationController.java
@@ -2,6 +2,7 @@ package com.capgemini.ccsw.estimador.estimation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,16 +19,22 @@ import com.capgemini.ccsw.estimador.estimation.model.EstimationSearchDto;
 @RestController
 public class EstimationController {
 
-	@Autowired
-	EstimationService estimationService;
-	
-	@Autowired
-	BeanMapper beanMapper;
-	
-	@RequestMapping(path="", method = RequestMethod.POST)
-	public Page<EstimationDto> findPage(@RequestBody EstimationSearchDto dto) {
-	
-		return this.beanMapper.mapPage(estimationService.findPage(dto), EstimationDto.class);
-	}
+  @Autowired
+  EstimationService estimationService;
+
+  @Autowired
+  BeanMapper beanMapper;
+
+  @RequestMapping(path = "", method = RequestMethod.POST)
+  public Page<EstimationDto> findPage(@RequestBody EstimationSearchDto dto) {
+
+    return this.beanMapper.mapPage(this.estimationService.findPage(dto), EstimationDto.class);
+  }
+
+  @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+  public EstimationDto getEstimation(@PathVariable Long id) {
+
+    return this.beanMapper.map(this.estimationService.getEstimation(id), EstimationDto.class);
+  }
 
 }
--- src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationService.java
+++ src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationService.java
@@ -2,19 +2,22 @@ package com.capgemini.ccsw.estimador.estimation;
 
 import org.springframework.data.domain.Page;
 
-import com.capgemini.ccsw.estimador.estimation.model.EstimationSearchDto;
 import com.capgemini.ccsw.estimador.estimation.model.EstimationEntity;
+import com.capgemini.ccsw.estimador.estimation.model.EstimationSearchDto;
 
 /**
  * @author iciudade
  */
 public interface EstimationService {
-	
-	/**
-	 * Metodo para recuperar un listado paginado de {@link com.capgemini.ccsw.estimador.estimation.model.EstimationEntity}
-	 * @param dto
-	 * @return
-	 */
-	Page<EstimationEntity> findPage(EstimationSearchDto dto);
+
+  /**
+   * Metodo para recuperar un listado paginado de {@link com.capgemini.ccsw.estimador.estimation.model.EstimationEntity}
+   *
+   * @param dto
+   * @return
+   */
+  Page<EstimationEntity> findPage(EstimationSearchDto dto);
+
+  EstimationEntity getEstimation(Long id);
 
 }
