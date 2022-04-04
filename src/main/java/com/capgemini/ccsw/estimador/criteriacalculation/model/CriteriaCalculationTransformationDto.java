a880da69574d2fec41cbc8616e19aab658bf6ef0
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2022-04-04 13:32:04 +0200

    Finished

---
+++ src/main/java/com/capgemini/ccsw/estimador/criteriacalculation/model/CriteriaCalculationTransformationDto.java
@@ -0,0 +1,24 @@
+package com.capgemini.ccsw.estimador.criteriacalculation.model;
+
+public class CriteriaCalculationTransformationDto {
+
+	private Double hours;
+	
+	private String concept;
+
+	public Double getHours() {
+		return hours;
+	}
+
+	public void setHours(Double hours) {
+		this.hours = hours;
+	}
+
+	public String getConcept() {
+		return concept;
+	}
+
+	public void setConcept(String concept) {
+		this.concept = concept;
+	}
+}
