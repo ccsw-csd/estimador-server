a39e545b9811071a2933a78cfc72409bfed1fd1f
CORP\asolerpa <andrea.soler-paz-external@capgemini.com>
2022-05-13 13:03:59 +0200

    Mostrar solo última versión en el listado

--- src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationRepository.java
+++ src/main/java/com/capgemini/ccsw/estimador/estimation/EstimationRepository.java
@@ -27,14 +27,15 @@ public interface EstimationRepository extends CrudRepository<EstimationEntity, L
      */
     @Query("select e FROM EstimationEntity e " + "WHERE (:customer is null or e.project.customer.id = :customer) "
             + "and (:project is null or e.project.name like '%'||:project||'%') "
-            + "and (:startDate is null or :endDate is null or e.created BETWEEN :startDate and :endDate)")
+            + "and (:startDate is null or :endDate is null or e.created BETWEEN :startDate and :endDate) and (e.lastUpdate = "
+            + "(select max(lastUpdate) from EstimationEntity e2 where e2.project = e.project and e2.createdBy = e.createdBy))")
     @EntityGraph(attributePaths = { "project", "project.customer", "createdBy", "createdBy.role" })
     Page<EstimationEntity> find(@Param("customer") Long customer, @Param("project") String project,
             @Param("startDate") Date startDate, @Param("endDate") Date endDate, Pageable pageable);
 
     EstimationEntity getById(Long id);
-    
+
     EstimationEntity findFirstByProjectCustomerOrderByLastUpdateDesc(CustomerEntity customer);
-    
+
     List<EstimationEntity> findByProjectIdOrderByLastUpdateDesc(Long projectId);
 }
